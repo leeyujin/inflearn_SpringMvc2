b606d83e1aa8fba24128bad948da34d160c3cdea
yujinlee <now8567@hanmail.net>
2022-10-30 20:48:38 +0900

    converter

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,20 @@
+package hello.typeconverter.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService(){
+        // 등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 사용
+        conversionService.convert()
+    }
+}
